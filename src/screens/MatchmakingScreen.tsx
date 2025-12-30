3386adb1615bd16889f46c5013a0d8481e7c5978
Brailey <bgonza214@calstatela.edu>
2025-12-30 13:47:03 -0800

    Match making loading screen UI

--- src/screens/MatchmakingScreen.tsx
+++ src/screens/MatchmakingScreen.tsx
@@ -1,9 +1,49 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { useEffect } from "react";
 
 export default function MatchmakingScreen() {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    // Matchmaking delay
+    const timer = setTimeout(() => {
+      navigation.navigate("Match" as never);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <View>
-      <Text>Matchmaking Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Finding Opponent ...</Text>
+
+      <ActivityIndicator
+        size="large"
+        color="#007AF"
+        style={{ marginTop: 20 }}
+      />
+
+      <Text style={styles.subtitle}>Searching for a player to duel</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    padding: 24,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "700",
+  },
+  subtitle: {
+    marginTop: 16,
+    fontSize: 16,
+    color: "#555",
+  },
+});
